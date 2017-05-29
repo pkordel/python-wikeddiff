c5c4248e450b1107dc275a79392efa09080a8637
Peter Kordel <pkordel@gmail.com>
2017-05-29 09:56:37 +0700

    fix: add correct doc type, fix string concatenations

--- WikEdDiff/HtmlFormatter.py
+++ WikEdDiff/HtmlFormatter.py
@@ -28,7 +28,6 @@ class HtmlFormatter:
     # version since the formatted diff is inserted directly into the page in the
     # browser.
     fullHtmlTemplate = """
-<?xml version="1.0" encoding="UTF-8"?>
 <!doctype html>
 <html>
 <head>
@@ -75,89 +74,89 @@ class HtmlFormatter:
 }
 .wikEdDiffBlock { }
 .wikEdDiffBlock0 { background-color: #ffff80; }
-.wikEdDiffBlock1 { background-color: #d0ff80; } 
-.wikEdDiffBlock2 { background-color: #ffd8f0; } 
-.wikEdDiffBlock3 { background-color: #c0ffff; } 
-.wikEdDiffBlock4 { background-color: #fff888; } 
-.wikEdDiffBlock5 { background-color: #bbccff; } 
-.wikEdDiffBlock6 { background-color: #e8c8ff; } 
-.wikEdDiffBlock7 { background-color: #ffbbbb; } 
-.wikEdDiffBlock8 { background-color: #a0e8a0; } 
+.wikEdDiffBlock1 { background-color: #d0ff80; }
+.wikEdDiffBlock2 { background-color: #ffd8f0; }
+.wikEdDiffBlock3 { background-color: #c0ffff; }
+.wikEdDiffBlock4 { background-color: #fff888; }
+.wikEdDiffBlock5 { background-color: #bbccff; }
+.wikEdDiffBlock6 { background-color: #e8c8ff; }
+.wikEdDiffBlock7 { background-color: #ffbbbb; }
+.wikEdDiffBlock8 { background-color: #a0e8a0; }
 .wikEdDiffBlockHighlight {
-    background-color: #777; color: #fff; 
-    border: solid #777; border-width: 1px 0; 
-} 
+    background-color: #777; color: #fff;
+    border: solid #777; border-width: 1px 0;
+}
 
 /* Mark */
 .wikEdDiffMarkLeft, .wikEdDiffMarkRight {
-    font-weight: bold; background-color: #ffe49c; 
-    color: #666; border-radius: 0.25em; padding: 0.2em; margin: 0 1px; 
-} 
-.wikEdDiffMarkLeft:before { content: "{cssMarkLeft}"; } 
-.wikEdDiffMarkRight:before { content: "{cssMarkRight}"; } 
-.wikEdDiffMarkLeft.wikEdDiffNoUnicode:before { content: "<"; } 
-.wikEdDiffMarkRight.wikEdDiffNoUnicode:before { content: ">"; } 
-.wikEdDiffMark { background-color: #e8e8e8; color: #666; } 
-.wikEdDiffMark0 { background-color: #ffff60; } 
-.wikEdDiffMark1 { background-color: #c8f880; } 
-.wikEdDiffMark2 { background-color: #ffd0f0; } 
-.wikEdDiffMark3 { background-color: #a0ffff; } 
-.wikEdDiffMark4 { background-color: #fff860; } 
-.wikEdDiffMark5 { background-color: #b0c0ff; } 
-.wikEdDiffMark6 { background-color: #e0c0ff; } 
-.wikEdDiffMark7 { background-color: #ffa8a8; } 
-.wikEdDiffMark8 { background-color: #98e898; } 
-.wikEdDiffMarkHighlight { background-color: #777; color: #fff; } 
+    font-weight: bold; background-color: #ffe49c;
+    color: #666; border-radius: 0.25em; padding: 0.2em; margin: 0 1px;
+}
+.wikEdDiffMarkLeft:before { content: "{cssMarkLeft}"; }
+.wikEdDiffMarkRight:before { content: "{cssMarkRight}"; }
+.wikEdDiffMarkLeft.wikEdDiffNoUnicode:before { content: "<"; }
+.wikEdDiffMarkRight.wikEdDiffNoUnicode:before { content: ">"; }
+.wikEdDiffMark { background-color: #e8e8e8; color: #666; }
+.wikEdDiffMark0 { background-color: #ffff60; }
+.wikEdDiffMark1 { background-color: #c8f880; }
+.wikEdDiffMark2 { background-color: #ffd0f0; }
+.wikEdDiffMark3 { background-color: #a0ffff; }
+.wikEdDiffMark4 { background-color: #fff860; }
+.wikEdDiffMark5 { background-color: #b0c0ff; }
+.wikEdDiffMark6 { background-color: #e0c0ff; }
+.wikEdDiffMark7 { background-color: #ffa8a8; }
+.wikEdDiffMark8 { background-color: #98e898; }
+.wikEdDiffMarkHighlight { background-color: #777; color: #fff; }
 
 /* Wrappers */
-.wikEdDiffContainer { } 
+.wikEdDiffContainer { }
 .wikEdDiffFragment {
-    white-space: pre-wrap; background: #fff; border: #bbb solid; 
-    border-width: 1px 1px 1px 0.5em; border-radius: 0.5em; font-family: sans-serif; 
-    font-size: 88%; line-height: 1.6; box-shadow: 2px 2px 2px #ddd; padding: 1em; margin: 0; 
-} 
+    white-space: pre-wrap; background: #fff; border: #bbb solid;
+    border-width: 1px 1px 1px 0.5em; border-radius: 0.5em; font-family: sans-serif;
+    font-size: 88%; line-height: 1.6; box-shadow: 2px 2px 2px #ddd; padding: 1em; margin: 0;
+}
 .wikEdDiffNoChange {
-    background: #f0f0f0; border: 1px #bbb solid; border-radius: 0.5em; 
-    line-height: 1.6; box-shadow: 2px 2px 2px #ddd; padding: 0.5em; margin: 1em 0; 
-    text-align: center; 
-} 
-.wikEdDiffSeparator { margin-bottom: 1em; } 
-.wikEdDiffOmittedChars { } 
+    background: #f0f0f0; border: 1px #bbb solid; border-radius: 0.5em;
+    line-height: 1.6; box-shadow: 2px 2px 2px #ddd; padding: 0.5em; margin: 1em 0;
+    text-align: center;
+}
+.wikEdDiffSeparator { margin-bottom: 1em; }
+.wikEdDiffOmittedChars { }
 
 /* Newline */
-.wikEdDiffNewline:before { content: "¶"; color: transparent; } 
-.wikEdDiffBlock:hover .wikEdDiffNewline:before { color: #aaa; } 
-.wikEdDiffBlockHighlight .wikEdDiffNewline:before { color: transparent; } 
-.wikEdDiffBlockHighlight:hover .wikEdDiffNewline:before { color: #ccc; } 
-.wikEdDiffBlockHighlight:hover .wikEdDiffInsert .wikEdDiffNewline:before, 
+.wikEdDiffNewline:before { content: "¶"; color: transparent; }
+.wikEdDiffBlock:hover .wikEdDiffNewline:before { color: #aaa; }
+.wikEdDiffBlockHighlight .wikEdDiffNewline:before { color: transparent; }
+.wikEdDiffBlockHighlight:hover .wikEdDiffNewline:before { color: #ccc; }
+.wikEdDiffBlockHighlight:hover .wikEdDiffInsert .wikEdDiffNewline:before,
 .wikEdDiffInsert:hover .wikEdDiffNewline:before
-{ color: #999; } 
-.wikEdDiffBlockHighlight:hover .wikEdDiffDelete .wikEdDiffNewline:before, 
+{ color: #999; }
+.wikEdDiffBlockHighlight:hover .wikEdDiffDelete .wikEdDiffNewline:before,
 .wikEdDiffDelete:hover .wikEdDiffNewline:before
-{ color: #aaa; } 
+{ color: #aaa; }
 
 /* Tab */
-.wikEdDiffTab { position: relative; } 
-.wikEdDiffTabSymbol { position: absolute; top: -0.2em; } 
-.wikEdDiffTabSymbol:before { content: "→"; font-size: smaller; color: #ccc; } 
-.wikEdDiffBlock .wikEdDiffTabSymbol:before { color: #aaa; } 
-.wikEdDiffBlockHighlight .wikEdDiffTabSymbol:before { color: #aaa; } 
-.wikEdDiffInsert .wikEdDiffTabSymbol:before { color: #aaa; } 
-.wikEdDiffDelete .wikEdDiffTabSymbol:before { color: #bbb; } 
+.wikEdDiffTab { position: relative; }
+.wikEdDiffTabSymbol { position: absolute; top: -0.2em; }
+.wikEdDiffTabSymbol:before { content: "→"; font-size: smaller; color: #ccc; }
+.wikEdDiffBlock .wikEdDiffTabSymbol:before { color: #aaa; }
+.wikEdDiffBlockHighlight .wikEdDiffTabSymbol:before { color: #aaa; }
+.wikEdDiffInsert .wikEdDiffTabSymbol:before { color: #aaa; }
+.wikEdDiffDelete .wikEdDiffTabSymbol:before { color: #bbb; }
 
 /* Space */
-.wikEdDiffSpace { position: relative; } 
-.wikEdDiffSpaceSymbol { position: absolute; top: -0.2em; left: -0.05em; } 
-.wikEdDiffSpaceSymbol:before { content: "·"; color: transparent; } 
-.wikEdDiffBlock:hover .wikEdDiffSpaceSymbol:before { color: #999; } 
-.wikEdDiffBlockHighlight .wikEdDiffSpaceSymbol:before { color: transparent; } 
-.wikEdDiffBlockHighlight:hover .wikEdDiffSpaceSymbol:before { color: #ddd; } 
+.wikEdDiffSpace { position: relative; }
+.wikEdDiffSpaceSymbol { position: absolute; top: -0.2em; left: -0.05em; }
+.wikEdDiffSpaceSymbol:before { content: "·"; color: transparent; }
+.wikEdDiffBlock:hover .wikEdDiffSpaceSymbol:before { color: #999; }
+.wikEdDiffBlockHighlight .wikEdDiffSpaceSymbol:before { color: transparent; }
+.wikEdDiffBlockHighlight:hover .wikEdDiffSpaceSymbol:before { color: #ddd; }
 .wikEdDiffBlockHighlight:hover .wikEdDiffInsert .wikEdDiffSpaceSymbol:before,
-.wikEdDiffInsert:hover .wikEdDiffSpaceSymbol:before 
-{ color: #888; } 
+.wikEdDiffInsert:hover .wikEdDiffSpaceSymbol:before
+{ color: #888; }
 .wikEdDiffBlockHighlight:hover .wikEdDiffDelete .wikEdDiffSpaceSymbol:before,
-.wikEdDiffDelete:hover .wikEdDiffSpaceSymbol:before 
-{ color: #999; } 
+.wikEdDiffDelete:hover .wikEdDiffSpaceSymbol:before
+{ color: #999; }
 
 /* Error */
 .wikEdDiffError .wikEdDiffFragment,
@@ -323,31 +322,31 @@ var wikEdDiffBlockHandler = function ( event, element, type ) {
         'deleteEnd': '</span>',
 
         'blockStart':
-                '<span class="wikEdDiffBlock"' +
-                'title="{title}" id="wikEdDiffBlock{number}"' +
+                '<span class="wikEdDiffBlock" ' +
+                'title="{title}" id="wikEdDiffBlock{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');">',
         'blockColoredStart':
-                '<span class="wikEdDiffBlock wikEdDiffBlock wikEdDiffBlock{number}"' +
-                'title="{title}" id="wikEdDiffBlock{number}"' +
+                '<span class="wikEdDiffBlock wikEdDiffBlock wikEdDiffBlock{number}" ' +
+                'title="{title}" id="wikEdDiffBlock{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');">',
         'blockEnd': '</span>',
 
         'markLeft':
-                '<span class="wikEdDiffMarkLeft{nounicode}"' +
-                'title="{title}" id="wikEdDiffMark{number}"' +
+                '<span class="wikEdDiffMarkLeft{nounicode}" ' +
+                'title="{title}" id="wikEdDiffMark{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');"></span>',
         'markLeftColored':
-                '<span class="wikEdDiffMarkLeft{nounicode} wikEdDiffMark wikEdDiffMark{number}"' +
-                'title="{title}" id="wikEdDiffMark{number}"' +
+                '<span class="wikEdDiffMarkLeft{nounicode} wikEdDiffMark wikEdDiffMark{number}" ' +
+                'title="{title}" id="wikEdDiffMark{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');"></span>',
 
         'markRight':
-                '<span class="wikEdDiffMarkRight{nounicode}"' +
-                'title="{title}" id="wikEdDiffMark{number}"' +
+                '<span class="wikEdDiffMarkRight{nounicode}" ' +
+                'title="{title}" id="wikEdDiffMark{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');"></span>',
         'markRightColored':
-                '<span class="wikEdDiffMarkRight{nounicode} wikEdDiffMark wikEdDiffMark{number}"' +
-                'title="{title}" id="wikEdDiffMark{number}"' +
+                '<span class="wikEdDiffMarkRight{nounicode} wikEdDiffMark wikEdDiffMark{number}" ' +
+                'title="{title}" id="wikEdDiffMark{number}" ' +
                 'onmouseover="wikEdDiffBlockHandler(undefined, this, \'mouseover\');"></span>',
 
         'newline': '<span class="wikEdDiffNewline">\n</span>',
